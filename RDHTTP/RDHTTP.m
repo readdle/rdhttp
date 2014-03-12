fac47f38f9c65e70aadb0ee322ea3f8569a90c1a
Sergey Ischuk <falcon.ser@gmail.com>
2014-03-12 16:55:49 +0200

    - fixed build for armv64 (removed warnings)

--- RDHTTP/RDHTTP.m
+++ RDHTTP/RDHTTP.m
@@ -220,10 +220,10 @@ static char *const RDHTTPDispatchQueueActive = "RDHTTPDispatchQueueKey";
 }
 
 - (NSString *)description {
-    return [NSString stringWithFormat:@"<RDHTTPResponse: URL %@ code %d length: %d>", 
+    return [NSString stringWithFormat:@"<RDHTTPResponse: URL %@ code %ld length: %lu>", 
             response.URL,
-            response.statusCode, 
-            [responseData length]];
+            (long)response.statusCode, 
+            (unsigned long)[responseData length]];
 }
 
 - (BOOL)  moveResponseFileToURL:(NSURL *)destination 
@@ -448,7 +448,7 @@ static char *const RDHTTPDispatchQueueActive = "RDHTTPDispatchQueueKey";
 
 - (void)setHTTPBodyData:(NSData *)data contentType:(NSString *)contentType {
     [self postBodyCheckAndSetContentType:contentType];
-    [urlRequest setValue:[NSString stringWithFormat:@"%u", [data length]] forHTTPHeaderField:@"Content-Length"];
+    [urlRequest setValue:[NSString stringWithFormat:@"%lu", (unsigned long)[data length]] forHTTPHeaderField:@"Content-Length"];
     [urlRequest setHTTPBody:data];
 }
 
@@ -795,7 +795,7 @@ static char *const RDHTTPDispatchQueueActive = "RDHTTPDispatchQueueKey";
                                                        multipartPostFields:multipartPostFiles
                                                                   encoding:encoding];
         
-        [request        addValue:[NSString stringWithFormat:@"%u", postStream.multipartBodyLength]
+        [request        addValue:[NSString stringWithFormat:@"%lu", (unsigned long)postStream.multipartBodyLength]
               forHTTPHeaderField:@"Content-Length"];
         
         NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; charset=%@; boundary=%@", charset, postStream.contentBoundary];
